9e46328a8612ed651e73a42f3b29694e5037ef28
Stanislav Pivovartsev <stanislav.pivovartsev@prym.com>
2024-07-09 12:37:38 +0200

    Fix definition of RabbitMQConsumer

--- src/MQConsumer/RabbitMQConsumer.php
+++ src/MQConsumer/RabbitMQConsumer.php
@@ -7,13 +7,15 @@ namespace StanislavPivovartsev\InterestingStatistics\Utils\MQConsumer;
 use PhpAmqpLib\Channel\AMQPChannel;
 use PhpAmqpLib\Connection\AbstractConnection;
 use PhpAmqpLib\Message\AMQPMessage;
+use StanislavPivovartsev\InterestingStatistics\Utils\Contract\AMQPMessageReceiverInterface;
 use StanislavPivovartsev\InterestingStatistics\Utils\Contract\MQConsumerInterface;
 
-abstract class RabbitMQConsumer implements MQConsumerInterface
+class RabbitMQConsumer implements MQConsumerInterface
 {
     public function __construct(
-        protected AbstractConnection $connection,
-        protected AMQPChannel    $channel,
+        private readonly AbstractConnection $connection,
+        private readonly AMQPChannel    $channel,
+        private readonly AMQPMessageReceiverInterface $amqpMessageReceiver,
     ) {
     }
 
@@ -23,7 +25,7 @@ abstract class RabbitMQConsumer implements MQConsumerInterface
     public function consume(string $queueName): void
     {
         $this->channel->basic_qos(0, 1, true);
-        $this->channel->basic_consume($queueName, '', false, false, false, false, [$this, 'onReceive']);
+        $this->channel->basic_consume($queueName, '', false, false, false, false, [$this->amqpMessageReceiver, 'onReceive']);
 
         try {
             $this->channel->consume();
@@ -34,6 +36,4 @@ abstract class RabbitMQConsumer implements MQConsumerInterface
         $this->channel->close();
         $this->connection->close();
     }
-
-    abstract protected function onReceive(AMQPMessage $message): void;
 }
