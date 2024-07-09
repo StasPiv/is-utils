27cedf5d9102a87b458a225f233dc6724aa1d115
Stanislav Pivovartsev <stanislav.pivovartsev@prym.com>
2024-07-09 22:56:59 +0200

    Add PgnParseModel

---
+++ src/Model/PgnParseModel.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace StanislavPivovartsev\InterestingStatistics\Utils\Model;
+
+class PgnParseModel extends AbstractMessageModel
+{
+    public function __construct(
+        protected readonly string $id,
+        protected readonly string $pgn,
+    ) {
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id,
+            'pgn' => $this->pgn,
+        ];
+    }
+
+    protected static function getRequiredKeys(): array
+    {
+        return ['id', 'pgn'];
+    }
+}
